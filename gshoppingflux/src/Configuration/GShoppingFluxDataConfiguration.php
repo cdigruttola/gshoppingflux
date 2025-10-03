7d559564672f6c9723c315df7f2651f23709fbb9
Carmine Di Gruttola <carmine.di.gruttola@pwc.com>
2025-10-03 16:04:39 +0200

    end of configuration

--- gshoppingflux/src/Configuration/GShoppingFluxDataConfiguration.php
+++ gshoppingflux/src/Configuration/GShoppingFluxDataConfiguration.php
@@ -36,6 +36,7 @@ class GShoppingFluxDataConfiguration extends AbstractMultistoreConfiguration
     public const GS_FILE_PREFIX = 'GS_FILE_PREFIX';
     public const GS_LOCAL_SHOP_CODE = 'GS_LOCAL_SHOP_CODE';
     public const GS_REVIEW_MODULES = 'GS_REVIEW_MODULES';
+    public const GS_AUTOEXPORT_ON_SAVE = 'GS_AUTOEXPORT_ON_SAVE';
 
     private const CONFIGURATION_FIELDS = [
         'product_type',
@@ -45,6 +46,25 @@ class GShoppingFluxDataConfiguration extends AbstractMultistoreConfiguration
         'shipping_country',
         'shipping_countries',
         'carriers_excluded',
+        'mpn_type',
+        'export_min_price',
+        'gender',
+        'age_group',
+        'colors',
+        'materials',
+        'patterns',
+        'sizes',
+        'export_attributes',
+        'no_gtin',
+        'shipping_dimension',
+        'no_brand',
+        'id_exists_tag',
+        'export_nap',
+        'quantity',
+        'featured_products',
+        'gen_file_in_root',
+        'file_prefix',
+        'autoexport_on_save',
     ];
 
     /**
@@ -59,7 +79,27 @@ class GShoppingFluxDataConfiguration extends AbstractMultistoreConfiguration
             ->setAllowedTypes('shipping_mode', 'string')
             ->setAllowedTypes('shipping_price', 'float')
             ->setAllowedTypes('shipping_country', 'string')
-            ->setAllowedTypes('shipping_countries', 'null|array');
+            ->setAllowedTypes('shipping_countries', 'null|array')
+            ->setAllowedTypes('mpn_type', 'string')
+            ->setAllowedTypes('export_min_price', 'float')
+            ->setAllowedTypes('gender', 'string')
+            ->setAllowedTypes('age_group', 'string')
+            ->setAllowedTypes('colors', 'null|array')
+            ->setAllowedTypes('materials', 'null|array')
+            ->setAllowedTypes('patterns', 'null|array')
+            ->setAllowedTypes('sizes', 'null|array')
+            ->setAllowedTypes('export_attributes', 'bool')
+            ->setAllowedTypes('no_gtin',  'bool')
+            ->setAllowedTypes('shipping_dimension',  'bool')
+            ->setAllowedTypes('no_brand',  'bool')
+            ->setAllowedTypes('id_exists_tag',  'bool')
+            ->setAllowedTypes('export_nap',  'bool')
+            ->setAllowedTypes('quantity',  'bool')
+            ->setAllowedTypes('featured_products',  'bool')
+            ->setAllowedTypes('gen_file_in_root',  'bool')
+            ->setAllowedTypes('file_prefix', 'string')
+            ->setAllowedTypes('autoexport_on_save',  'bool')
+            ->setAllowedTypes('store_code',  'bool');
     }
 
     /**
@@ -77,6 +117,26 @@ class GShoppingFluxDataConfiguration extends AbstractMultistoreConfiguration
         $return['shipping_country'] = $this->configuration->get(self::GS_SHIPPING_COUNTRY, null, $shopConstraint);
         $return['shipping_countries'] = (array) $this->configuration->get(self::GS_SHIPPING_COUNTRIES, null, $shopConstraint);
         $return['carriers_excluded'] = (array) $this->configuration->get(self::GS_CARRIERS_EXCLUDED, null, $shopConstraint);
+        $return['mpn_type'] = $this->configuration->get(self::GS_MPN_TYPE, null, $shopConstraint);
+        $return['export_min_price'] = $this->configuration->get(self::GS_EXPORT_MIN_PRICE, null, $shopConstraint);
+        $return['gender'] = $this->configuration->get(self::GS_GENDER, null, $shopConstraint);
+        $return['age_group'] = $this->configuration->get(self::GS_AGE_GROUP, null, $shopConstraint);
+        $return['colors'] = (array) $this->configuration->get(self::GS_COLOR, null, $shopConstraint);
+        $return['materials'] = (array) $this->configuration->get(self::GS_MATERIAL, null, $shopConstraint);
+        $return['patterns'] = (array) $this->configuration->get(self::GS_PATTERN, null, $shopConstraint);
+        $return['sizes'] = (array) $this->configuration->get(self::GS_SIZE, null, $shopConstraint);
+        $return['export_attributes'] = $this->configuration->get(self::GS_ATTRIBUTES, null, $shopConstraint);
+        $return['no_gtin'] = $this->configuration->get(self::GS_NO_GTIN, null, $shopConstraint);
+        $return['shipping_dimension'] = $this->configuration->get(self::GS_SHIPPING_DIMENSION, null, $shopConstraint);
+        $return['no_brand'] = $this->configuration->get(self::GS_NO_BRAND, null, $shopConstraint);
+        $return['id_exists_tag'] = $this->configuration->get(self::GS_ID_EXISTS_TAG, null, $shopConstraint);
+        $return['export_nap'] = $this->configuration->get(self::GS_EXPORT_NAP, null, $shopConstraint);
+        $return['quantity'] = $this->configuration->get(self::GS_QUANTITY, null, $shopConstraint);
+        $return['featured_products'] = $this->configuration->get(self::GS_FEATURED_PRODUCTS, null, $shopConstraint);
+        $return['gen_file_in_root'] = $this->configuration->get(self::GS_GEN_FILE_IN_ROOT, null, $shopConstraint);
+        $return['file_prefix'] = $this->configuration->get(self::GS_FILE_PREFIX, null, $shopConstraint);
+        $return['autoexport_on_save'] = $this->configuration->get(self::GS_AUTOEXPORT_ON_SAVE, null, $shopConstraint);
+        $return['store_code'] = $this->configuration->get(self::GS_LOCAL_SHOP_CODE, null, $shopConstraint);
 
         return $return;
     }
@@ -94,6 +154,26 @@ class GShoppingFluxDataConfiguration extends AbstractMultistoreConfiguration
         $this->updateConfigurationValue(self::GS_SHIPPING_COUNTRY, 'shipping_country', $configuration, $shopConstraint);
         $this->updateConfigurationValue(self::GS_SHIPPING_COUNTRIES, 'shipping_countries', $configuration, $shopConstraint);
         $this->updateConfigurationValue(self::GS_CARRIERS_EXCLUDED, 'carriers_excluded', $configuration, $shopConstraint);
+        $this->updateConfigurationValue(self::GS_MPN_TYPE, 'mpn_type', $configuration, $shopConstraint);
+        $this->updateConfigurationValue(self::GS_EXPORT_MIN_PRICE, 'export_min_price', $configuration, $shopConstraint);
+        $this->updateConfigurationValue(self::GS_GENDER, 'gender', $configuration, $shopConstraint);
+        $this->updateConfigurationValue(self::GS_AGE_GROUP, 'age_group', $configuration, $shopConstraint);
+        $this->updateConfigurationValue(self::GS_COLOR, 'colors', $configuration, $shopConstraint);
+        $this->updateConfigurationValue(self::GS_MATERIAL, 'materials', $configuration, $shopConstraint);
+        $this->updateConfigurationValue(self::GS_PATTERN, 'patterns', $configuration, $shopConstraint);
+        $this->updateConfigurationValue(self::GS_SIZE, 'sizes', $configuration, $shopConstraint);
+        $this->updateConfigurationValue(self::GS_ATTRIBUTES, 'export_attributes', $configuration, $shopConstraint);
+        $this->updateConfigurationValue(self::GS_NO_GTIN, 'no_gtin', $configuration, $shopConstraint);
+        $this->updateConfigurationValue(self::GS_SHIPPING_DIMENSION, 'shipping_dimension', $configuration, $shopConstraint);
+        $this->updateConfigurationValue(self::GS_NO_BRAND, 'no_brand', $configuration, $shopConstraint);
+        $this->updateConfigurationValue(self::GS_ID_EXISTS_TAG, 'id_exists_tag', $configuration, $shopConstraint);
+        $this->updateConfigurationValue(self::GS_EXPORT_NAP, 'export_nap', $configuration, $shopConstraint);
+        $this->updateConfigurationValue(self::GS_QUANTITY, 'quantity', $configuration, $shopConstraint);
+        $this->updateConfigurationValue(self::GS_FEATURED_PRODUCTS, 'featured_products', $configuration, $shopConstraint);
+        $this->updateConfigurationValue(self::GS_GEN_FILE_IN_ROOT, 'gen_file_in_root', $configuration, $shopConstraint);
+        $this->updateConfigurationValue(self::GS_FILE_PREFIX, 'file_prefix', $configuration, $shopConstraint);
+        $this->updateConfigurationValue(self::GS_AUTOEXPORT_ON_SAVE, 'autoexport_on_save', $configuration, $shopConstraint);
+        $this->updateConfigurationValue(self::GS_LOCAL_SHOP_CODE, 'store_code', $configuration, $shopConstraint);
 
         return [];
     }
--- gshoppingflux/src/Form/GShoppingFluxConfigurationType.php
+++ gshoppingflux/src/Form/GShoppingFluxConfigurationType.php
@@ -30,7 +30,9 @@ use cdigruttola\GShoppingFlux\Configuration\GShoppingFluxDataConfiguration;
 use ImageType;
 use PrestaShop\PrestaShop\Core\Form\ChoiceProvider\ImageTypeChoiceProvider;
 use PrestaShopBundle\Entity\Repository\ImageTypeRepository;
+use PrestaShopBundle\Form\Admin\Type\FeatureChoiceType;
 use PrestaShopBundle\Form\Admin\Type\MultistoreConfigurationType;
+use PrestaShopBundle\Form\Admin\Type\SwitchType;
 use PrestaShopBundle\Form\Admin\Type\TranslatableType;
 use PrestaShopBundle\Form\Admin\Type\TranslatorAwareType;
 use PrestaShopBundle\Form\FormHelper;
@@ -85,6 +87,11 @@ class GShoppingFluxConfigurationType extends TranslatorAwareType
             $this->trans('Generate shipping costs in several countries [EXPERIMENTAL]', 'Modules.Gshoppingflux.Admin') => 'full',
         ];
 
+        $mpn_types = [
+            $this->trans('Reference', 'Modules.Gshoppingflux.Admin') => 'reference',
+            $this->trans('Supplier Reference', 'Modules.Gshoppingflux.Admin') => 'supplier_reference',
+        ];
+
         $imageTypes = [];
         $dbImageTypes = $this->imageTypeRepository->findBy(['products' => true]);
 
@@ -121,7 +128,7 @@ class GShoppingFluxConfigurationType extends TranslatorAwareType
                 'multistore_configuration_key' => GShoppingFluxDataConfiguration::GS_SHIPPING_COUNTRY,
             ])
             ->add('shipping_countries', ChoiceType::class, [
-                'label' => $this->trans('Shipping country', 'Modules.Gshoppingflux.Admin'),
+                'label' => $this->trans('Shipping countries', 'Modules.Gshoppingflux.Admin'),
                 'choices' => $this->countryChoices,
                 'multiple' => true,
                 'multistore_configuration_key' => GShoppingFluxDataConfiguration::GS_SHIPPING_COUNTRIES,
@@ -136,6 +143,100 @@ class GShoppingFluxConfigurationType extends TranslatorAwareType
                 'label' => $this->trans('Images type', 'Modules.Gshoppingflux.Admin'),
                 'choices' => $imageTypes,
                 'multistore_configuration_key' => GShoppingFluxDataConfiguration::GS_IMG_TYPE,
+            ])
+            ->add('mpn_type', ChoiceType::class, [
+                'label' => $this->trans('Manufacturers References type (MPN)', 'Modules.Gshoppingflux.Admin'),
+                'choices' => $mpn_types,
+                'multistore_configuration_key' => GShoppingFluxDataConfiguration::GS_MPN_TYPE,
+            ])
+            ->add('export_min_price', MoneyType::class, [
+                'label' => $this->trans('Minimum product price', 'Modules.Gshoppingflux.Admin'),
+                'help' => $this->trans('Products at lower price are not exported. Enter 0.00 for no use.', 'Modules.Gshoppingflux.Admin'),
+                'attr' => ['data-display-price-precision' => FormHelper::DEFAULT_PRICE_PRECISION],
+                'multistore_configuration_key' => GShoppingFluxDataConfiguration::GS_EXPORT_MIN_PRICE,
+            ])
+            ->add('gender', FeatureChoiceType::class, [
+                'label' => $this->trans('Products gender feature', 'Modules.Gshoppingflux.Admin'),
+                'multistore_configuration_key' => GShoppingFluxDataConfiguration::GS_GENDER,
+            ])
+            ->add('age_group', FeatureChoiceType::class, [
+                'label' => $this->trans('Products age group feature', 'Modules.Gshoppingflux.Admin'),
+                'multistore_configuration_key' => GShoppingFluxDataConfiguration::GS_AGE_GROUP,
+            ])
+            ->add('colors', FeatureChoiceType::class, [
+                'label' => $this->trans('Products color feature', 'Modules.Gshoppingflux.Admin'),
+                'multiple' => true,
+                'multistore_configuration_key' => GShoppingFluxDataConfiguration::GS_COLOR,
+            ])
+            ->add('materials', FeatureChoiceType::class, [
+                'label' => $this->trans('Products material feature', 'Modules.Gshoppingflux.Admin'),
+                'multiple' => true,
+                'multistore_configuration_key' => GShoppingFluxDataConfiguration::GS_MATERIAL,
+            ])
+            ->add('patterns', FeatureChoiceType::class, [
+                'label' => $this->trans('Products pattern feature', 'Modules.Gshoppingflux.Admin'),
+                'multiple' => true,
+                'multistore_configuration_key' => GShoppingFluxDataConfiguration::GS_PATTERN,
+            ])
+            ->add('sizes', FeatureChoiceType::class, [
+                'label' => $this->trans('Products size feature', 'Modules.Gshoppingflux.Admin'),
+                'multiple' => true,
+                'multistore_configuration_key' => GShoppingFluxDataConfiguration::GS_SIZE,
+            ])
+            ->add('export_attributes', SwitchType::class, [
+                'label' => $this->trans('Export attributes combinations', 'Modules.Gshoppingflux.Admin'),
+                'help' => $this->trans('If checked, one product is exported for each attributes combination. Products should have at least one attribute filled in order to be exported as combinations.', 'Modules.Gshoppingflux.Admin'),
+                'multistore_configuration_key' => GShoppingFluxDataConfiguration::GS_ATTRIBUTES,
+            ])
+            ->add('no_gtin', SwitchType::class, [
+                'label' => $this->trans('Export products with no GTIN code', 'Modules.Gshoppingflux.Admin'),
+                'help' => $this->trans('Allow export of products, that no not have a GTIN code (EAN13/UPC)', 'Modules.Gshoppingflux.Admin'),
+                'multistore_configuration_key' => GShoppingFluxDataConfiguration::GS_NO_GTIN,
+            ])
+            ->add('shipping_dimension', SwitchType::class, [
+                'label' => $this->trans('Export products shipping dimensions', 'Modules.Gshoppingflux.Admin'),
+                'help' => $this->trans('Allow export of dimension for each products, if typed in product details', 'Modules.Gshoppingflux.Admin'),
+                'multistore_configuration_key' => GShoppingFluxDataConfiguration::GS_SHIPPING_DIMENSION,
+            ])
+            ->add('no_brand', SwitchType::class, [
+                'label' => $this->trans('Export products with no brand', 'Modules.Gshoppingflux.Admin'),
+                'help' => $this->trans('Allow export of products, that no not have a brand (Manufacturer)', 'Modules.Gshoppingflux.Admin'),
+                'multistore_configuration_key' => GShoppingFluxDataConfiguration::GS_NO_BRAND,
+            ])
+            ->add('id_exists_tag', SwitchType::class, [
+                'label' => $this->trans('Set <identifier_exists> tag to FALSE', 'Modules.Gshoppingflux.Admin'),
+                'help' => $this->trans('If your product is new (which you submit through the condition attribute) and it doesn’t have a gtin and brand or mpn and brand.', 'Modules.Gshoppingflux.Admin'),
+                'multistore_configuration_key' => GShoppingFluxDataConfiguration::GS_ID_EXISTS_TAG,
+            ])
+            ->add('export_nap', SwitchType::class, [
+                'label' => $this->trans('Export non-available products', 'Modules.Gshoppingflux.Admin'),
+                'multistore_configuration_key' => GShoppingFluxDataConfiguration::GS_EXPORT_NAP,
+            ])
+            ->add('quantity', SwitchType::class, [
+                'label' => $this->trans('Export product quantity', 'Modules.Gshoppingflux.Admin'),
+                'multistore_configuration_key' => GShoppingFluxDataConfiguration::GS_QUANTITY,
+            ])
+            ->add('featured_products', SwitchType::class, [
+                'label' => $this->trans('Export "On Sale" indication', 'Modules.Gshoppingflux.Admin'),
+                'multistore_configuration_key' => GShoppingFluxDataConfiguration::GS_FEATURED_PRODUCTS,
+            ])
+            ->add('gen_file_in_root', SwitchType::class, [
+                'label' => $this->trans('Generate the files to the root of the site', 'Modules.Gshoppingflux.Admin'),
+                'multistore_configuration_key' => GShoppingFluxDataConfiguration::GS_GEN_FILE_IN_ROOT,
+            ])
+            ->add('file_prefix', TextType::class, [
+                'label' => $this->trans('Prefix for output filename', 'Modules.Gshoppingflux.Admin'),
+                'help' => $this->trans('Allows you to prefix feed filename. Makes it a little harder for other to guess your feed names', 'Modules.Gshoppingflux.Admin'),
+                'multistore_configuration_key' => GShoppingFluxDataConfiguration::GS_FILE_PREFIX,
+            ])
+            ->add('autoexport_on_save', SwitchType::class, [
+                'label' => $this->trans('Automatic export on saves?', 'Modules.Gshoppingflux.Admin'),
+                'help' => $this->trans('When disabled, you have to "Save & Export" manually or run the CRON job, to generate new files.', 'Modules.Gshoppingflux.Admin'),
+                'multistore_configuration_key' => GShoppingFluxDataConfiguration::GS_AUTOEXPORT_ON_SAVE,
+            ])
+            ->add('store_code', TextType::class, [
+                'label' => $this->trans('Your store code', 'Modules.Gshoppingflux.Admin'),
+                'multistore_configuration_key' => GShoppingFluxDataConfiguration::GS_LOCAL_SHOP_CODE,
             ]);
     }
 
